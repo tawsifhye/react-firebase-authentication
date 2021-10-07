2ef82343854e6a7c515ee490c3cf56467ce6c684
tawsifhye <tawsif.lucse@gmail.com>
2021-10-07 16:09:46 +0600

    New User Using Email And Password

--- src/Firebase/firebase.confog.js
+++ src/Firebase/firebase.confog.js
@@ -1,10 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAXX7b7EIyig8wv9HnL2Zr4mkT9lWQthqk",
-    authDomain: "react-firebase-authentic-1042a.firebaseapp.com",
-    projectId: "react-firebase-authentic-1042a",
-    storageBucket: "react-firebase-authentic-1042a.appspot.com",
-    messagingSenderId: "914415492474",
-    appId: "1:914415492474:web:6d7847424906edfbcc0ac3"
-  };
+const firebaseConfig = {};
 
-  export default firebaseConfig;
+export default firebaseConfig;
