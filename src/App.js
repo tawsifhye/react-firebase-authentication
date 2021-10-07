c0ff3fa1027a75d65b910bacad99ceb3b2b38c74
tawsifhye <tawsif.lucse@gmail.com>
2021-10-07 15:07:28 +0600

    Bootstrap Form  And Get User Email And Password

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import './App.css';
-import initializeAuthentication from './Firebase/firebase.initialize';
+import { useState } from "react";
+import "./App.css";
+import initializeAuthentication from "./Firebase/firebase.initialize";
 
 initializeAuthentication();
 const GoogleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const auth = getAuth();
-  const handleGoogleSignIn = () =>{
-    signInWithPopup(auth, GoogleProvider)
-    .then(result => {
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, GoogleProvider).then((result) => {
       const user = result.user;
     });
-  }
+  };
 
-  const handleRegistration = e =>{
-    console.log("preeeeeeeeeeeeeeeeees");
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRegistration = (e) => {
+    console.log(email, password);
     e.preventDefault();
-  }
+  };
   return (
-    <div className="App">
+    <div className="mx-5 my-2">
       <form onSubmit={handleRegistration}>
-        <h3>Please Register</h3>
-        <label htmlFor="email">Email: </label>
-        <input type="text" name='email' />
-        <br />
-        <label htmlFor="password">Password: </label>
-        <input type="password" name="password" id=""/>
-        <br />
-        <input type="submit" value="Register" />
+        <h3 className="text-primary">Please Register</h3>
+        <div className="row mb-3">
+          <label for="inputEmail3" className="col-sm-2 col-form-label">
+            Email
+          </label>
+          <div className="col-sm-10">
+            <input
+              onBlur={handleEmailChange}
+              type="email"
+              className="form-control"
+              id="inputEmail3"
+            />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label for="inputPassword3" className="col-sm-2 col-form-label">
+            Password
+          </label>
+          <div className="col-sm-10">
+            <input
+              onBlur={handlePasswordChange}
+              type="password"
+              className="form-control"
+              id="inputPassword3"
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign in
+        </button>
       </form>
       <br /> <br /> <br />
       <div>--------------------------------------------------------</div>
